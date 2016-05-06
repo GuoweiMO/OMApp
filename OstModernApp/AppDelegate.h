533a327ef81f9277d955c790025f90b065341cf2
Guowei Mo <guowei.mo13@gamil.com>
2016-05-06 18:56:19 +0100

    Initial Commit

---
+++ OstModernApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OstModernApp
+//
+//  Created by Guowei Mo on 06/05/2016.
+//  Copyright © 2016 Guowei Mo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
